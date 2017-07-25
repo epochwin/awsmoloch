4df66d28890eb24458272a27e3dbfc39e2d37c85
Aaron <ajsmith10381@gmail.com>
2017-07-25 15:46:41 -0500

    cleaning up security groups

--- terraform/modules/moloch-env/security_groups.tf
+++ terraform/modules/moloch-env/security_groups.tf
@@ -1,30 +1,5 @@
-resource "aws_security_group" "pcap" {
-  name        = "pcap"
-  description = "Allow all traffic for analysis"
-  vpc_id = "${aws_vpc.moloch_vpc.id}"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.1.0/24"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["10.0.1.0/24"]
-  }
-
-  tags {
-    Name = "pcap"
-    Role = "security"
-  }
-}
-
-resource "aws_security_group" "allow_moloch" {
-  name        = "allow_ssh"
+resource "aws_security_group" "allow_viewer" {
+  name        = "allow_viewer"
   description = "Allow all inbound ssh and moloch web traffic"
   vpc_id = "${aws_vpc.moloch_vpc.id}"
 
@@ -40,6 +15,13 @@ resource "aws_security_group" "allow_moloch" {
     to_port     = 8005
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   egress {
